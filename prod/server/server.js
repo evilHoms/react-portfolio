a595c7bd72ba59418320954866d5484df6533522
evilHoms <homspage@gmail.com>
2018-03-24 19:02:55 +0300

    Openshift fixes

--- prod/server/server.js
+++ prod/server/server.js
@@ -4,30 +4,21 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
-var _config = require('../../config.json');
-
-var _config2 = _interopRequireDefault(_config);
-
-var _routes = require('./routes.js');
-
-var _routes2 = _interopRequireDefault(_routes);
-
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//const MongoClient = mongodb.MongoClient;
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || _config2.default.port;
 //import mongodb from 'mongodb';
+//import config from '../../config.json';
+// import routes from './routes.js';
+// import path from 'path';
 
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || _config2.default.ip;
+//const MongoClient = mongodb.MongoClient;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || config.port || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || config.ip || '0.0.0.0';
 
 var app = (0, _express2.default)();
-app.use("/public", _express2.default.static(_path2.default.resolve(__dirname, 'public')));
-app.get('/', function (req, res) {
-  res.send('hello');
-});
+// app.use("/public", express.static(path.resolve(__dirname, 'public')));
+// app.get('/', function(req, res) {
+//   res.send('hello');
+// })
 //routes(app, MongoClient);
 app.listen(port, ip);
