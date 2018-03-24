cd72ea0bb96c0beb9afe05e594544c95b146dbea
evilHoms <homspage@gmail.com>
2018-03-24 18:11:28 +0300

    Checking openshift

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import mongodb from 'mongodb';
+//import mongodb from 'mongodb';
 import config from '../../config.json';
 import routes from './routes.js';
 import path from 'path';
 
-const MongoClient = mongodb.MongoClient;
+//const MongoClient = mongodb.MongoClient;
 const port     = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || config.port;
 const ip       = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || config.ip;
 
 const app = express();
 app.use("/public", express.static(path.resolve(__dirname, 'public')));
-routes(app, MongoClient);
-const server = app.listen(port, ip);
+app.get('/', function(req, res) {
+  res.send('hello');
+})
+//routes(app, MongoClient);
+app.listen(port, ip);
