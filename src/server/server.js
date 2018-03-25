d4505edcb4082bad9cdf02aaba9fe087a259a74e
evilHoms <homspage@gmail.com>
2018-03-25 15:04:35 +0300

    added favicon

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongodb from 'mongodb';
+import favicon from 'serve-favicon';
 import config from '../../config.json';
 import routes from './routes.js';
 import path from 'path';
@@ -9,5 +10,6 @@ const port = process.env.PORT || config.port || 8080;
 
 const app = express();
 app.use("/public", express.static(path.resolve(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 routes(app, MongoClient);
 app.listen(port);
